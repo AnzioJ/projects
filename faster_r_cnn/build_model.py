9686114367b3d5232cd7bebeae5b6e582f9cb46d
AnzioJ <149542939+AnzioJ@users.noreply.github.com>
2023-12-12 18:57:35 -0500

    Update build_model.py

--- faster_r_cnn/build_model.py
+++ faster_r_cnn/build_model.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.model import Model
 
 # 1. Base Convolutional Network
 def create_base_network(input_shape):
     input_layer = Input(shape=input_shape)
     
-    # Example architecture
+    # Architecture
     x = Conv2D(64, (3, 3), activation='relu', padding='same')(input_layer)
     x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D((2, 2))(x)
@@ -31,11 +32,11 @@ def create_rpn(feature_map, num_anchors):
 
     # Classification Layer - Object or Not
     cls = Conv2D(num_anchors * 2, (1, 1), padding='valid', activation='sigmoid')(shared_map)
-    cls = Flatten()(cls)  # Flatten for classification
+    cls = Flatten()(cls)  
 
     # Regression Layer - Bounding Box Adjustment
     reg = Conv2D(num_anchors * 4, (1, 1), padding='valid')(shared_map)
-    reg = Flatten()(reg)  # Flatten for regression
+    reg = Flatten()(reg)  
 
     # Create a Keras model
     model = tf.keras.Model(inputs=feature_map, outputs=[cls, reg])
@@ -74,6 +75,8 @@ class ROIPoolingLayer(tf.keras.layers.Layer):
 
         final_output = tf.concat(outputs, axis=0)
         return final_output
+    
+
 
 # 4. Classification and Bounding Box Regression Network
 def create_classification_and_regression_network(input_shape, num_classes):
@@ -106,33 +109,104 @@ def create_classification_and_regression_network(input_shape, num_classes):
 
     return model
 
-# Combine Components
-input_shape = (256, 256, 3)
-num_classes = 21  # Example number of classes (including background)
-num_anchors = 9  # Example number of anchors
+def apply_nms(predictions, iou_threshold=0.5):
+    """
+    Apply Non-Maximum Suppression to prediction bounding boxes.
+
+    :param predictions: A list of predictions, where each prediction is a tuple (box, score, class).
+    :param iou_threshold: The Intersection over Union (IoU) threshold to use for determining overlap.
+    :return: A list of filtered predictions after applying NMS.
+    """
+    filtered_predictions = []
+
+    # Sort the predictions by score in descending order
+    predictions = sorted(predictions, key=lambda x: x[1], reverse=True)
+
+    while predictions:
+        # Select the prediction with the highest score and remove it from the list
+        best_prediction = predictions.pop(0)
+        filtered_predictions.append(best_prediction)
+
+        predictions = [pred for pred in predictions if pred[2] != best_prediction[2] or 
+                       iou(best_prediction[0], pred[0]) < iou_threshold]
+
+    return filtered_predictions
+
+def iou(box1, box2):
+    """
+    Calculate the Intersection over Union (IoU) of two bounding boxes.
+
+    :param box1: Coordinates of the first box (x1, y1, x2, y2).
+    :param box2: Coordinates of the second box (x1, y1, x2, y2).
+    :return: IoU value.
+    """
+    # Calculate intersection area
+    xi1 = max(box1[0], box2[0])
+    yi1 = max(box1[1], box2[1])
+    xi2 = min(box1[2], box2[2])
+    yi2 = min(box1[3], box2[3])
+    inter_area = max(xi2 - xi1, 0) * max(yi2 - yi1, 0)
+
+    # Calculate each box area
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    # Calculate union area
+    union_area = box1_area + box2_area - inter_area
+
+    # Calculate IoU
+    iou = inter_area / union_area
+
+    return iou
+
+# Input Shape
+input_shape = (256, 256, 3) 
+
+# Number of anchors for RPN
+num_anchors = 9
+
+# Number of classes (including background)
+num_classes = 2
 
 # Create Base Network
 base_network = create_base_network(input_shape)
 
-# Create RPN
-feature_map_input = Input(shape=[None, None, 128])  # Output shape from the base network
-rpn_model = create_rpn(feature_map_input, num_anchors)
+# Feature Map Input for RPN 
+feature_map_shape = base_network.output_shape[1:]
+feature_map_input = tf.keras.Input(shape=feature_map_shape)
 
-# Create RoI Pooling Layer
+# Create RPN
+rpn = create_rpn(feature_map_input, num_anchors)
+
+# RoI Pooling Layer
 roi_pooling_layer = ROIPoolingLayer(pooled_height=7, pooled_width=7)
 
-# Create Classification and Regression Network
-classification_and_regression_model = create_classification_and_regression_network((7, 7, 128), num_classes)
+# Classification and Regression Network
+classification_and_regression_network = create_classification_and_regression_network((7, 7, feature_map_shape[-1]), num_classes)
 
-# Example Input for the Combined Model
-image_input = Input(shape=input_shape)
+# Model Input and Output
+image_input = tf.keras.Input(shape=input_shape)
+
+# Get feature map
 feature_map = base_network(image_input)
 
-# Note: In a full implementation, RPN outputs would be connected to RoI pooling, 
-# and RoI pooling outputs would be connected to the classification and regression network.
-# This requires additional logic to handle the dynamic nature of proposals.
+# Get RPN outputs
+rpn_cls, rpn_reg = rpn(feature_map)
 
-# This setup shows the sequential flow of components.
-# Further integration and training logic is needed for a functional model.
+# Placeholder for RoIs input
+rois_input = tf.keras.Input(shape=(None, 4)) 
 
-combined_model = Model(inputs=image_input, outputs=[feature_map, rpn_model.output, classification_and_regression_model.output])
+# RoI Pooling
+pooled_features = roi_pooling_layer([feature_map, rois_input])
+
+# Classification and Regression outputs
+cls_output, reg_output = classification_and_regression_network(pooled_features)
+
+# Create the model
+faster_rcnn_model = tf.keras.Model(inputs=[image_input, rois_input], outputs=[rpn_cls, rpn_reg, cls_output, reg_output])
+
+# Compile the model
+faster_rcnn_model.compile(optimizer='adam', 
+                          loss={'classification_output': 'categorical_crossentropy', 
+                                'bbox_regression_output': 'mean_squared_error'},
+                          metrics={'classification_output': 'accuracy'})
